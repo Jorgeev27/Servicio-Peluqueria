ce22f164fe9e372acd591a5f5bd7a8210d63c872
Jorge Escobar <jorgeescobarv27@gmail.com>
2023-04-04 12:39:35 +0100

    Main

--- source/frontend/index.php
+++ source/frontend/index.php
@@ -14,7 +14,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.php">
-                <img src="../img/logoPeluqueria.png" alt="logoPeluqueria" width="50" height="50">
+                <img src="../assets/img/logoPeluqueria.png" alt="logoPeluqueria" width="50" height="50">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -73,7 +73,7 @@
     <main>
         <div class="row">
             <div class="col-12 col-md-12 col-lg-12">
-                <img src="../img/imgMain.jpeg" class="img-fluid" alt="imgMain">
+                <img src="../assets/img/imgMain.jpeg" class="img-fluid" alt="imgMain">
             </div>
         </div>
     </main>
