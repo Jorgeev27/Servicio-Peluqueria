b544cdbd736be4e1c04c88d63c297b850f6f4284
Jorge Escobar <jorgeescobarv27@gmail.com>
2023-04-25 18:18:34 +0100

    Main

--- source/frontend/index.php
+++ source/frontend/index.php
@@ -1,7 +1,6 @@
 <?php
     declare(strict_types = 1);
     require("../backend/config/config.php");
-    require_once("../.htaccess");
     require_once("../backend/model/BasePDOConnection.php");
 ?>
 <!DOCTYPE html>
